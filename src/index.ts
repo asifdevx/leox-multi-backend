e316842871841a2974036fd758f3b1ab8d9e2e99
asifdevx <xashadulasif@gmail.com>
2025-09-21 19:17:55 +0600

    backend of leox_multi

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import dotenv from "dotenv";
+import { nftSchema} from "./graphql/schemas/nft.schema";
+ import connetdb from "./config/connectdb";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.use(express.json());
+
+const corsOptions = {
+  origin: "http://leox_multi.vercel.app", 
+  credentials: true, // enable CORS with credentials
+};
+app.use(cors(corsOptions));
+// Main backend data
+app.get("/", (req, res) => {
+  res.send("Welcome to the GraphQL API!");
+});
+app.use(
+  "/g",
+  graphqlHTTP({
+    schema: nftSchema,
+    graphiql: true,
+  })
+);
+
+const start = async () => {
+  try {
+    await connetdb()
+    app.listen(PORT, () => {
+      console.log(`Server is running....`);
+    });
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
+};
+
+start();
